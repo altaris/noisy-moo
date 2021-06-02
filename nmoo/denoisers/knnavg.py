80d7f14810c2e9e710a9ebf90672b033d7c53c7d
Cédric HT <nope-mcnopeface@asdf.org>
2021-06-02 18:18:59 +0900

    Completed and documented KNNAvg algo

--- nmoo/denoisers/knnavg.py
+++ nmoo/denoisers/knnavg.py
@@ -3,14 +3,12 @@ A denoiser tries to cancel noise. (also water is wet)
 """
 __docformat__ = "google"
 
-from typing import Optional
-
 from pymoo.model.problem import Problem
 from scipy.spatial.distance import cdist
 import numpy as np
 import pandas as pd
 
-from nmoo.utils import *
+from nmoo.utils import ProblemWrapper
 
 
 class KNNAvg(ProblemWrapper):
@@ -23,15 +21,15 @@ class KNNAvg(ProblemWrapper):
     """
 
     _distance_weight_mode: str
-    _max_distance: Optional[float]
+    _max_distance: float
     _n_neighbors: int
 
     def __init__(
         self,
         problem: Problem,
-        distance_weight_type: str = "uniform",
-        max_distance: Optional[float] = None,
+        max_distance: float,
         n_neighbors: int = 5,  # KNN
+        distance_weight_type: str = "uniform",
     ):
         """
         Constructor.
@@ -39,7 +37,7 @@ class KNNAvg(ProblemWrapper):
         Args:
             problem (:obj:`Problem`): Noisy pymoo problem.
             distance_weight_type (str): Either "squared" or "uniform".
-            max_distance (Optional[float]): Distance cutoff.
+            max_distance (float): Distance cutoff.
             n_neighbors (int): Number of neighbors to consider (KNN).
         """
         super().__init__(problem)
@@ -51,7 +49,7 @@ class KNNAvg(ProblemWrapper):
             )
         self._distance_weight_mode = distance_weight_type
 
-        if max_distance is not None and max_distance < 0.0:
+        if max_distance < 0.0:
             raise ValueError(
                 "Parameter max_distance must either be 'None' or >= 0."
             )
@@ -67,40 +65,47 @@ class KNNAvg(ProblemWrapper):
         """
         self._problem._evaluate(x, out, *args, **kwargs)
         for i, sol in enumerate(x):
-            df = pd.DataFrame(self._problem._history["x"])
-            df["_sed"] = cdist(
+            # Store the solution history into a dataframe (note that we are
+            # using the wrapped problem's history to make sure this dataframe
+            # is never empty).
+            x_hist = pd.DataFrame(self._problem._history["x"])
+            # Compute the standardized Euclidean distances between the current
+            # solution (sol) and all historical solutions.
+            x_hist["_sed"] = cdist(
                 self._problem._history["x"],
                 sol.reshape((1, -1)),
                 "seuclidean",
             )
-            max_distance = (
-                self._max_distance
-                if self._max_distance is not None
-                else df["_sed"].max() + 1.0
-                # TODO: Check how to handle self._max_distance == None
-            )
-            df = (
-                df[df["_sed"] <= max_distance]
+            # Apply the KNN scheme: select the K closest neighbors among those
+            # closer than the maximum allowed distance.
+            x_hist = (
+                x_hist[x_hist["_sed"] <= self._max_distance]
                 .sort_values(by="_sed")
                 .head(self._n_neighbors)
             )
-            if df.shape[0] <= 1:
+            if x_hist.shape[0] <= 1:
+                # If only the current solution remains, then skip to the next
+                # solution.
                 continue
+            # Compute the weights.
             if self._distance_weight_mode == "squared":
-                df["_w"] = (max_distance - df["_sed"]) ** 2
+                x_hist["_w"] = (self._max_distance - x_hist["_sed"]) ** 2
             elif self._distance_weight_mode == "uniform":
-                df["_w"] = 1.0
+                x_hist["_w"] = 1.0
             else:
                 raise RuntimeError(
                     "Unknown distance weight mode: "
                     + self._distance_weight_mode
                 )
-            # TODO: Generalize
-            df_F = pd.DataFrame(self._problem._history["F"])
-            avg = np.average(
-                df_F.iloc[df.index].to_numpy(),
-                axis=0,
-                weights=df["_w"].to_numpy(),
-            )
-            out["F"][i] = avg
+            # Compute the weighted averages of the (numerical) outputs.
+            for k in out:
+                if not isinstance(out[k], np.ndarray):
+                    continue
+                out_k_hist = pd.DataFrame(self._problem._history[k])
+                avg = np.average(
+                    out_k_hist.iloc[x_hist.index],
+                    axis=0,
+                    weights=x_hist["_w"],
+                )
+                out[k][i] = avg
         self.add_to_history_x_out(x, out)
