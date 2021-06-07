5502069ba583eaed38ccbf5dda583cea92780c7c
Cédric HT <nope-mcnopeface@asdf.org>
2021-06-07 09:58:51 +0900

    Added Delayer wrapper

---
+++ nmoo/delayer.py
@@ -0,0 +1,40 @@
+"""
+A problem that incurs a constant delay at each `_evaluate` call.
+"""
+__docformat__ = "google"
+
+from time import sleep
+
+from pymoo.model.problem import Problem
+import numpy as np
+
+from nmoo.wrappedproblem import WrappedProblem
+
+
+class Delayer(WrappedProblem):
+    """
+    A problem that sleeps for a set amount of time at each `_evaluate` call,
+    passing calling the wrapped problem's `_evaluate`.
+    """
+
+    _delay: float
+    """
+    Sleep time in seconds. Floating point number may be used to indicate a more
+    precise sleep time.
+    """
+
+    def __init__(self, problem: Problem, delay: float = 0.05):
+        super().__init__(problem)
+
+        if delay < 0.0:
+            raise ValueError("Delay must be a positive.")
+        self._delay = delay
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        sleep(self._delay)
+        self._problem._evaluate(x, out, *args, **kwargs)
+        self.add_to_history_x_out(
+            x,
+            out,
+            delay=np.full((x.shape[0],), self._delay),
+        )
