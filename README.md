219c4596b70ab487b1b99655b77e16a2972c7a2e
Cédric HT <nope-mcnopeface@asdf.org>
2021-06-08 17:37:56 +0900

    Packaging

--- README.md
+++ README.md
@@ -4,14 +4,23 @@ noisy-moo
 <img src="https://github.com/altaris/noisy-moo/raw/main/imgs/the_cow.png"
 alt="The Cow" width="500"/>
 
-An abstract pipeline for noisy multi-objective optimization, building upon [the
+A wrapper-based framework for pymoo problem modification. Motivated by [the
 works](https://github.com/ERATOMMSD/QUATIC2021-KNN-Averaging) of
 [Klikovits](https://klikovits.net) and
 [Arcaini](http://group-mmm.org/~arcaini/).
 
-# Getting started
+# Installation
+
+Simply run
+```sh
+pip install git+https://github.com/altaris/noisy-moo.git
+```
+
+## Getting started
 
 See [example.ipynb](/example.ipynb) for a quick example.
+
+# Contributing
 
 ## Dependencies
 
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split()
+
+packages = (
+    ["nmoo"]
+    + ["nmoo." + p for p in setuptools.find_packages(where="./nmoo")]
+)
+
+setuptools.setup(
+    author="Cédric HT",
+    author_email="hothanh@nii.ac.jp",
+    description="A wrapper-based framework for pymoo problem modification.",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Mathematics"
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="nmoo",
+    packages=packages,
+    platforms="any",
+    project_urls={
+        "Issues": "https://github.com/altaris/noisy-moo/issues",
+    },
+    python_requires=">=3.7",
+    url="https://github.com/altaris/noisy-moo",
+    version="1.0.0",
+)
