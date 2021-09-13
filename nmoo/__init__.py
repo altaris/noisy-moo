9f329b824ffff270959542a2cba0ddb027794571
Cédric HT <altaris@users.noreply.github.com>
2021-09-13 17:16:49 +0900

    Defined nmoo.__version__
    Thanks to Martijn Pieters https://stackoverflow.com/a/17638236

--- nmoo/__init__.py
+++ nmoo/__init__.py
@@ -4,8 +4,15 @@
 """
 __docformat__ = "google"
 
+from pkg_resources import get_distribution, DistributionNotFound
+
 from .wrapped_problem import WrappedProblem
 from . import benchmark
 from . import denoisers
 from . import noises
 from . import utils
+
+try:
+    __version__ = get_distribution('nmoo').version
+except DistributionNotFound:
+    __version__ = "local"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 name = "nmoo"
-version = "3.1.0"
+version = "3.1.1"
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
